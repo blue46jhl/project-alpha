6b6bccb39c11e42b55f428fd5d252e49d7b907eb
blue46jhl <blue46jhl@gmail.com>
2020-01-07 18:16:08 -0800

    register

--- forumapp/src/App.js
+++ forumapp/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Register from "./components/register.js";
+import Login from "./components/login.js";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: "",
+      dbResponse: ""
+    };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+        .then((resp) => resp.text())
+        .then((resp) => this.setState({ apiResponse: resp }))
+        .catch(error => error);
+  }
+
+  callDB() {
+    fetch("http://localhost:9000/testDB")
+        .then((resp) => resp.text())
+        .then((resp) => this.setState({ dbResponse: resp }))
+        .catch(error => error);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+      this.callDB();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" target="_blank">
+              <img src={logo} width="60" height="60" alt="React" />
+            </a>
+            <Link to="/" className="navbar-brand">Forum App </Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/register" className="nav-link">Register</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/login" className="nav-link">Login</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/register" exact component={Register} />
+          <Route path="/login/:id" component={Login} />
+          <p className="App-intro"> {this.state.apiResponse}</p>
+          <p className="App-intro"> {this.state.dbResponse}</p>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
